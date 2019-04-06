d70ac0cb782dcf1b5473059dd362506f764d8840
Mohammed <m7amd.s3ad@hotmail.com>
2019-04-06 14:03:08 +0300

    fixing float

--- src/Components/Brands/index.js
+++ src/Components/Brands/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as actionCreators from "../../store/actions";
-import BrandsList from "./BrandsList";
+import BrandsList from "./brandsList";
 
 class Brands extends Component {
   render() {
