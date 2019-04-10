52da5c46cd1e2d3a799cf11038c674bc1120d839
aljumiah <aljumiah@outlook.com>
2019-04-10 14:11:28 +0300

    Qcode is done

---
+++ src/Components/ItemDetail/VariationDetail.js
@@ -0,0 +1,181 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actionCreators from "../../store/actions";
+import { Link } from "react-router-dom";
+import imageNotFound from "../../assets/images/notfound.png";
+
+class VariationDetail extends Component {
+  state = {};
+
+  //   onVaraiteChange = id => {
+  //     this.props.setVaraition(id);
+  //   };
+  //   IncrementItem = () => {
+  //     this.setState(prevState => {
+  //       if (prevState.quantity < 9) {
+  //         return {
+  //           quantity: prevState.quantity + 1
+  //         };
+  //       } else {
+  //         return null;
+  //       }
+  //     });
+  //   };
+  //   DecreaseItem = () => {
+  //     this.setState(prevState => {
+  //       if (prevState.quantity > 1) {
+  //         return {
+  //           quantity: prevState.quantity - 1
+  //         };
+  //       } else {
+  //         return null;
+  //       }
+  //     });
+  //   };
+
+  componentDidMount() {
+    this.props.fetchVariatons();
+  }
+
+  render() {
+    const varaiteID = this.props.match.params.varaiteID;
+    // const varaite = this.props.varaite;
+    console.log("from Qcode", this.props.variatons);
+
+    let variatonsList = this.props.variatons.map(
+      varaite =>
+        varaite.id === +varaiteID && (
+          <div
+            className="form-check"
+            style={{
+              padding: 20
+            }}
+          >
+            <input
+              className="form-check-input"
+              type="radio"
+              name="varaition"
+              id={`Radios-${varaite.id}`}
+              value={`option-${varaite.id}`}
+              onChange={() => this.onVaraiteChange(varaite.id)}
+            />
+            <div className="col-12">
+              <div className="col-3" style={{}}>
+                {varaite.image ? (
+                  <img style={{ width: "100%" }} src={varaite.image} />
+                ) : (
+                  <img style={{ width: "100%" }} src={imageNotFound} />
+                )}
+              </div>
+              <ul className="list-group list-group-horizontal-sm col-9">
+                <li className="list-group-item col-6">
+                  <ul className="list-group list-group-horizontal-sm">
+                    <li className="list-group-item" style={{ width: "50%" }}>
+                      Size:
+                    </li>
+                    <li className="list-group-item " style={{ width: "50%" }}>
+                      {varaite.size}
+                    </li>
+                  </ul>
+                  <ul className="list-group list-group-horizontal-sm">
+                    <li className="list-group-item" style={{ width: "50%" }}>
+                      Color
+                    </li>
+                    <li className="list-group-item" style={{ width: "50%" }}>
+                      {varaite.color}
+                    </li>
+                  </ul>
+                </li>
+                <li className="list-group-item col-6">
+                  <ul className="list-group list-group-horizontal-sm">
+                    <li className="list-group-item" style={{ width: "100%" }}>
+                      Price
+                    </li>
+                  </ul>
+                  <ul className="list-group list-group-horizontal-sm col-12">
+                    <li
+                      className="list-group-item "
+                      style={{ width: "100%", fontWeight: 400 }}
+                    >
+                      {varaite.price}{" "}
+                      <span
+                        className="text-success"
+                        style={{ fontWeight: 800 }}
+                      >
+                        SAR
+                      </span>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+              <div className="col-9">hiQcode</div>
+            </div>
+            {/* <div className="col-12 animated fadeInRight">
+                <div className="col-7">
+                  <button
+                    className="btn btn-success m-1"
+                    onClick={this.IncrementItem}
+                  >
+                    +
+                  </button>
+                  <input
+                    style={{ width: "20%", textAlign: "center" }}
+                    className="inputne m-1"
+                    value={this.state.quantity}
+                    onChange={this.handleChange}
+                  />
+                  <button
+                    className="btn btn-danger m-1"
+                    onClick={this.DecreaseItem}
+                  >
+                    -
+                  </button>
+                </div>
+                <div className="col-4">
+                  <a
+                    href="#"
+                    className="btn btn-primary  m-1"
+                    style={{
+                      width: "100%",
+                      background: "#40a9c3",
+                      color: "#fff",
+                      borderColor: "#40a9c3"
+                    }}
+                  >
+                    Add To Cart
+                  </a>
+                </div>
+              </div> */}
+          </div>
+        )
+    );
+    if (!this.props.variatons) {
+      return <div className="col-12">There is no variation</div>;
+    } else {
+      return (
+        <div style={{ padding: 20 }}>
+          <div className="col-12" style={{ background: "#fff" }}>
+            {variatonsList}
+          </div>
+        </div>
+      );
+    }
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    variatons: state.variatonsReducer.variatons
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchVariatons: () => dispatch(actionCreators.fetchVariatons())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(VariationDetail);
--- src/Components/Navigation/Search.js
+++ src/Components/Navigation/Search.js
@@ -3,9 +3,15 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import * as actionCreators from "../../store/actions";
 class Search extends Component {
+  state = {
+    flag: false
+  };
   handleChange = event => {
     this.props.onSearch(event.target.value);
-    this.setState({ query: event.target.value });
+    this.setState({ query: event.target.value, flag: true });
+  };
+  hideSearch = () => {
+    this.setState({ flag: false });
   };
   render() {
     console.log(this.props.searchfiltered);
@@ -21,13 +27,19 @@ class Search extends Component {
         />
         <dic>
           {this.props.searchfiltered.length > 0 &&
-          this.state.query.length > 0 ? (
+          this.state.query.length > 0 &&
+          this.state.flag ? (
             <div
               className=" col-12 searchBar animated fadeInDown Shadow "
               style={{}}
             >
               {this.props.searchfiltered.map(item => (
-                <Link className="col-12" to={`/item/${item.id}`} key={item.id}>
+                <Link
+                  className="col-12"
+                  to={`/item/${item.id}`}
+                  key={item.id}
+                  onClick={this.hideSearch}
+                >
                   <div
                     className="hoverSearch col-12"
                     style={{ padding: 10, borderBottom: "1px solid #e7e7e7" }}
@@ -40,7 +52,7 @@ class Search extends Component {
               ))}
             </div>
           ) : (
-            <div />
+            <div> </div>
           )}
         </dic>
       </>
